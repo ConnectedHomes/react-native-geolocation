a7f382b59d84fc5d12081133ec10d21714631876
domthomhive <dominic.thomas@hivehome.com>
2022-06-14 14:16:18 +0100

    Trying current location for android 8

--- android/src/main/java/co/uk/hive/reactnativegeolocation/location/LocationController.java
+++ android/src/main/java/co/uk/hive/reactnativegeolocation/location/LocationController.java
@@ -4,23 +4,34 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 
 import androidx.annotation.MainThread;
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 
 import com.annimon.stream.Stream;
 import com.annimon.stream.function.Function;
+import com.google.android.gms.location.CurrentLocationRequest;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.Granularity;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.Priority;
 import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.TaskExecutors;
+
+import java.util.Objects;
 
 public class LocationController {
     private final Context mContext;
@@ -38,24 +49,41 @@ public class LocationController {
     public void getCurrentPosition(CurrentPositionRequest currentPositionRequest,
                                    Function<LatLng, Object> successCallback,
                                    Function<Object, Object> failureCallback) {
-        if (mLocationClient == null) {
-            failureCallback.apply(LocationError.LOCATION_UNKNOWN);
-            return;
+        if (android.os.Build.VERSION.SDK_INT <= Build.VERSION_CODES.O) {
+            // TODO: Try current position for Android 8? Also, try build on device etc... also try play services 20?
+            final CurrentLocationRequest currentLocationRequest = new CurrentLocationRequest.Builder()
+                    .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+                    .setGranularity(Granularity.GRANULARITY_FINE)
+                    .setDurationMillis(3000).build();
+
+            // TODO: How do we use the cancellation token? do we need it?
+            final Task<Location> currentLocationTask = mLocationClient.getCurrentLocation(currentLocationRequest, null);
+            currentLocationTask.addOnSuccessListener(location -> successCallback.apply(new LatLng(location.getLatitude(), location.getLongitude())));
+            currentLocationTask.addOnFailureListener(e -> {
+                Log.e(LocationController.class.getName(), Objects.requireNonNull(e.getMessage()));
+                failureCallback.apply(LocationError.LOCATION_UNKNOWN);
+            });
+
+        } else {
+            if (mLocationClient == null) {
+                failureCallback.apply(LocationError.LOCATION_UNKNOWN);
+                return;
+            }
+
+            if (!hasPermissions()) {
+                failureCallback.apply(LocationError.PERMISSION_DENIED);
+                return;
+            }
+
+            final LocationRequest locationRequest = getLocationRequest(currentPositionRequest);
+            LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                    .addLocationRequest(locationRequest);
+            SettingsClient client = LocationServices.getSettingsClient(mContext);
+
+            client.checkLocationSettings(builder.build())
+                    .addOnFailureListener(TaskExecutors.MAIN_THREAD, ignored -> failureCallback.apply(LocationError.LOCATION_UNKNOWN))
+                    .addOnSuccessListener(TaskExecutors.MAIN_THREAD, ignored -> requestLocation(currentPositionRequest, successCallback, failureCallback));
         }
-
-        if (!hasPermissions()) {
-            failureCallback.apply(LocationError.PERMISSION_DENIED);
-            return;
-        }
-
-        final LocationRequest locationRequest = getLocationRequest(currentPositionRequest);
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-                .addLocationRequest(locationRequest);
-        SettingsClient client = LocationServices.getSettingsClient(mContext);
-
-        client.checkLocationSettings(builder.build())
-                .addOnFailureListener(TaskExecutors.MAIN_THREAD, ignored -> failureCallback.apply(LocationError.LOCATION_UNKNOWN))
-                .addOnSuccessListener(TaskExecutors.MAIN_THREAD, ignored -> requestLocation(currentPositionRequest, successCallback, failureCallback));
     }
 
     private boolean hasPermissions() {
