442b8deb04c9c29c6b1235de5cc224c2ed7a5051
Dominic Thomas <dominic.thomas@hivehome.com>
2023-04-27 20:52:24 +0100

    WIP: Hard coded arriving and leaving notifications working

---
+++ android/sample-app/src/main/java/co/uk/hive/reactnativegeolocation/sample_app/Constants.java
@@ -0,0 +1,47 @@
+package co.uk.hive.reactnativegeolocation.sample_app;
+
+import java.util.HashMap;
+
+import co.uk.hive.reactnativegeolocation.location.LatLng;
+
+/**
+ * Constants used in this sample.
+ */
+
+final class Constants {
+
+    private Constants() {
+    }
+
+    private static final String PACKAGE_NAME = "com.google.android.gms.location.Geofence";
+
+    static final String GEOFENCES_ADDED_KEY = PACKAGE_NAME + ".GEOFENCES_ADDED_KEY";
+
+    /**
+     * Used to set an expiration time for a geofence. After this amount of time Location Services
+     * stops tracking the geofence.
+     */
+    private static final long GEOFENCE_EXPIRATION_IN_HOURS = 12;
+
+    /**
+     * For this sample, geofences expire after twelve hours.
+     */
+    static final long GEOFENCE_EXPIRATION_IN_MILLISECONDS =
+            GEOFENCE_EXPIRATION_IN_HOURS * 60 * 60 * 1000;
+    //static final float GEOFENCE_RADIUS_IN_METERS = 1609; // 1 mile, 1.6 km
+    static final float GEOFENCE_RADIUS_IN_METERS = 183; // 1 mile, 1.6 km
+
+    /**
+     * Map for storing information about airports in the San Francisco bay area.
+     */
+    static final HashMap<String, LatLng> GEO_FENCE_LANDMARKS = new HashMap<>();
+
+    static {
+//        // San Francisco International Airport.
+//        GEO_FENCE_LANDMARKS.put("SFO", new LatLng(37.621313, -122.378955));
+//        // Googleplex.
+//        GEO_FENCE_LANDMARKS.put("GOOGLE", new LatLng(37.422611,-122.0840577));
+        GEO_FENCE_LANDMARKS.put("ARRIVING", new LatLng(51.441846, -2.602087));
+        GEO_FENCE_LANDMARKS.put("LEAVING", new LatLng(51.441846, -2.602087));
+    }
+}
