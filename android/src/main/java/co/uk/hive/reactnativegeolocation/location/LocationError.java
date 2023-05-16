e5429fff34a833d2d7f5b9a4c0dfb8389f476845
Dominic Thomas <dominic.thomas@hivehome.com>
2023-05-16 11:04:31 +0100

    Adding api exception resolution attempt

--- android/src/main/java/co/uk/hive/reactnativegeolocation/location/LocationError.java
+++ android/src/main/java/co/uk/hive/reactnativegeolocation/location/LocationError.java
@@ -4,12 +4,12 @@ package co.uk.hive.reactnativegeolocation.location;
  * Mimics react-native-background-location's LocationError values.
  * https://transistorsoft.github.io/react-native-background-geolocation/modules/_react_native_background_geolocation_.html#locationerror
  */
-class LocationError {
-    static final int LOCATION_UNKNOWN = 0;
-    static final int PERMISSION_DENIED = 1;
-    static final int LOCATION_CLIENT_IS_NULL = 3;
-    static final int LOCATION_DISABLED = 4;
-    static final int LOCATION_IS_NULL = 5;
-    static final int CURRENT_LOCATION_FAILED = 6;
-    static final int LOCATION_SETTINGS_FAILED = 7;
+public class LocationError {
+    public static final int LOCATION_UNKNOWN = 0;
+    public static final int PERMISSION_DENIED = 1;
+    public static final int LOCATION_CLIENT_IS_NULL = 3;
+    public static final int LOCATION_DISABLED = 4;
+    public static final int LOCATION_IS_NULL = 5;
+    public static final int CURRENT_LOCATION_FAILED = 6;
+    public static final int LOCATION_SETTINGS_FAILED = 7;
 }
